52dab396ae7fa1133f2ca627bfa58639cb909e21
SJW-sys <58312729+SJW-sys@users.noreply.github.com>
2026-02-27 12:54:01 -0800

    updated script

--- Scripts/Project/folderdeployment.sh
+++ Scripts/Project/folderdeployment.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Ensure Mattermost structure exist
-mkdir -p /opt/docker/communicationplatformstack/DeploymentFiles/BindMounts/Mattermost/{config,data,logs,plugins,client/plugins,bleve-indexes}
+mkdir -p /opt/docker/Communication_Platform_Solution/DeploymentFiles/BindMounts/Mattermost/{config,data,logs,plugins,client/plugins,bleve-indexes}
 
 # ensure Caddy structure exist 
-mkdir -p /opt/docker/communicationplatformstack/DeploymentFiles/BindMounts/Caddy/Caddyfile
+mkdir -p /opt/docker/Communication_Platform_Solution/DeploymentFiles/BindMounts/Caddy/Caddyfile
 
 # ensure Postgres structure exist 
-mkdir -p /opt/docker/communicationplatformstack/DeploymentFiles/BindMounts/Postgres
+mkdir -p /opt/docker/Communication_Platform_Solution/DeploymentFiles/BindMounts/Postgres
 
 # ensure correct owner for bindmounts
-sudo chown "$USER":docker -R /opt/docker/communicationplatformstack/DeploymentFiles/BindMounts/
-sudo chown 2000:2000 -R /opt/docker/communicationplatformstack/DeploymentFiles/BindMounts/Mattermost/
+sudo chown "$USER":docker -R /opt/docker/Communication_Platform_Solution/DeploymentFiles/BindMounts/
+sudo chown 2000:2000 -R /opt/docker/Communication_Platform_Solution/DeploymentFiles/BindMounts/Mattermost/
 
 # ensure correct permissions of bindmounts
-sudo chmod 774 -R /opt/docker/communicationplatformstack/DeploymentFiles/BindMounts/
+sudo chmod 774 -R /opt/docker/Communication_Platform_Solution/DeploymentFiles/BindMounts/
